59a888d1a6222bac3f331e504c4037cb66efa157
dennyerikson <dennyeriks00on@gmail.com>
2019-01-17 16:23:35 -0200

    Add tabela no template produtos

--- appkraft/views.py
+++ appkraft/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
@@ -7,3 +8,10 @@ def home(request):
 
     context = {'msg':msg}
     return render(request, 'appkraft/home.html', context)
+
+def produtos(request):
+
+    produtos = Produtos.objects.all()
+
+    context = {'produtos':produtos}
+    return render(request, 'appkraft/produtos.html', context)
